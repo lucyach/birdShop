9edf4526358e725317ac0027b4792505a6d79cb4
Lucy Acheson <70671534+lucyach@users.noreply.github.com>
2021-03-20 20:18:47 -0700

    Add files via upload
    ALL WORKING CODE SO FAR. NO FUNCTIONAL PROPERTIES

---
+++ customerFiles.js
@@ -0,0 +1,13 @@
+var customers = {};
+// customer[1] = {}; // "pets"
+// dict[1][1] = {};
+// dict[foo][bar][buz] = {};
+// dict[foo][bar][buz][qux] = value;
+
+// customer[5101234567] = ["name", "pets" = {"name" = {"type", "feed", "produce", "status"}}, "boarding" = ["pet name", "date in", "date out"], "calls"]; // status: alive, dead, bought, on hold, waiting list
+
+
+
+function newCustomer() {
+    console.log("got it!");
+}
